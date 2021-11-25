e942059134ab34ba6bed831db4bff1b6664b3aa8
SKOTOS99 <felipd95@hotmail.com>
2021-11-25 15:24:59 -0600

    creacion de servicios productos: metodo listar y listar por id

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+ INSERT INTO productos (nombre,precio,create_at) VALUES ('PANASONIC',800,NOW());
+ INSERT INTO productos (nombre,precio,create_at) VALUES ('SONY',8040,NOW());
+ INSERT INTO productos (nombre,precio,create_at) VALUES ('HP',400,NOW());
+ INSERT INTO productos (nombre,precio,create_at) VALUES ('dell',8030,NOW());
+    
